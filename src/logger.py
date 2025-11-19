764d8c9162698c6ca783c75b63d3606f299df7b4
charan <charan4332@gmail.com>
2025-11-19 12:42:45 +0530

    Logging and Exception

---
+++ src/logger.py
@@ -0,0 +1,15 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE = f"log_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+LOG_DIR = os.path.join(os.getcwd(), "logs")
+os.makedirs(LOG_DIR, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format='[%(asctime)s] %(levelname)s - %(message)s',
+    level=logging.INFO,
+)
